3e332d79cf14ca0f02af57f4836daf27344e6c73
Vladimir Danilov <dev.danilovl@gmail.com>
2021-10-04 14:38:50 +0200

    cypress/plugins - add --disable-dev-shm-usage

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,6 +2,10 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+    on("before:browser:launch", (browser = {}, launchOptions) => {
+        if (browser.name === "chrome") {
+            launchOptions.args.push("--disable-dev-shm-usage");
+            return launchOptions;
+        }
+    })
 }
