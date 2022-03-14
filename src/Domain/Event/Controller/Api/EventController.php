80f7075953ecaec3a0652cd2923bb7540d2f2279
Vladimir Danilov <dev.danilovl@gmail.com>
2022-03-14 20:26:48 +0100

    Create event api endpoint.

---
+++ src/Domain/Event/Controller/Api/EventController.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+/*
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Domain\Event\Controller\Api;
+
+use App\Domain\Event\Http\Api\EventListHandle;
+use App\Domain\Work\Entity\Work;
+use Symfony\Component\HttpFoundation\{
+    Request,
+    JsonResponse
+};
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EventController extends AbstractController
+{
+    public function __construct(private EventListHandle $eventListHandle)
+    {
+    }
+
+    public function list(Request $request, Work $work): JsonResponse
+    {
+        return $this->eventListHandle->handle($request, $work);
+    }
+}
---
+++ src/Domain/Event/Http/Api/EventListHandle.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Domain\Event\Http\Api;
+
+use App\Domain\Work\Service\WorkDetailTabService;
+use Danilovl\ObjectToArrayTransformBundle\Service\ObjectToArrayTransformService;
+use App\Application\Constant\TabTypeConstant;
+use App\Domain\Work\Entity\Work;
+use Symfony\Component\HttpFoundation\{
+    Request,
+    JsonResponse
+};
+
+class EventListHandle
+{
+    public function __construct(
+        private WorkDetailTabService $workDetailTabService,
+        private ObjectToArrayTransformService $objectToArrayTransformService
+    ) {
+    }
+
+    public function handle(Request $request, Work $work): JsonResponse
+    {
+        $pagination = $this->workDetailTabService->getTabPagination($request, TabTypeConstant::TAB_EVENT, $work);
+
+        $events = [];
+        foreach ($pagination as $event) {
+            $events[] = $this->objectToArrayTransformService->transform('api_key_field', $event);
+        }
+
+        return new JsonResponse([
+            'count' => $pagination->count(),
+            'totalCount' => $pagination->getTotalItemCount(),
+            'success' => true,
+            'result' => $events
+        ]);
+    }
+}
