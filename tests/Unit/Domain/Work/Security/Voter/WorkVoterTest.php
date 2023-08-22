e800ab56cb3635b04f9f467dc1dddf21d85420b4
Vladimir Danilov <dev.danilovl@gmail.com>
2023-08-22 11:10:00 +0200

    Create work Security test.

---
+++ tests/Unit/Domain/Work/Security/Voter/WorkVoterTest.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+/*
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Domain\Work\Security\Voter;
+
+use App\Application\Constant\VoterSupportConstant;
+use App\Domain\Work\Entity\Work;
+use App\Domain\Work\Security\Voter\WorkVoter;
+use App\Domain\Work\Service\WorkService;
+use PHPUnit\Framework\TestCase;
+
+class WorkVoterTest extends TestCase
+{
+    private readonly WorkVoter $workVoter;
+
+    protected function setUp(): void
+    {
+        $this->workVoter = new class(new WorkService) extends WorkVoter {
+            public function supportsPublic(string $attribute, mixed $subject): bool
+            {
+                return $this->supports($attribute, $subject);
+            }
+        };
+    }
+
+    public function testSupports(): void
+    {
+        $work = $this->createMock(Work::class);
+
+        $this->assertTrue($this->workVoter->supportsPublic(VoterSupportConstant::VIEW->value, $work));
+        $this->assertTrue($this->workVoter->supportsPublic(VoterSupportConstant::EDIT->value, $work));
+        $this->assertTrue($this->workVoter->supportsPublic(VoterSupportConstant::DELETE->value, $work));
+
+        $this->assertFalse($this->workVoter->supportsPublic('invalid_attribute', $work));
+        $this->assertFalse($this->workVoter->supportsPublic(VoterSupportConstant::VIEW->value, 'invalid_subject'));
+    }
+}
