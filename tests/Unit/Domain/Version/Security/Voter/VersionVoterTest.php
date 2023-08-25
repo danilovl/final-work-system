60bb3121b17c5b41f001ae3c87c7b3b27067dfed
Vladimir Danilov <dev.danilovl@gmail.com>
2023-08-25 11:10:00 +0200

    Create Version security test.

---
+++ tests/Unit/Domain/Version/Security/Voter/VersionVoterTest.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+/*
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Domain\Version\Security\Voter;
+
+use App\Application\Constant\VoterSupportConstant;
+use App\Domain\Version\Security\Voter\Subject\VersionVoterSubject;
+use App\Domain\Version\Security\Voter\VersionVoter;
+use PHPUnit\Framework\TestCase;
+
+class VersionVoterTest extends TestCase
+{
+    private readonly VersionVoter $versionVoter;
+
+    protected function setUp(): void
+    {
+        $this->versionVoter = new class extends VersionVoter {
+            public function supportsPublic(string $attribute, mixed $subject): bool
+            {
+                return $this->supports($attribute, $subject);
+            }
+        };
+    }
+
+    public function testSupports(): void
+    {
+        $versionVoterSubject = $this->createMock(VersionVoterSubject::class);
+
+        $supports = [
+            VoterSupportConstant::CREATE->value,
+            VoterSupportConstant::VIEW->value,
+            VoterSupportConstant::EDIT->value,
+            VoterSupportConstant::DOWNLOAD->value,
+            VoterSupportConstant::DELETE->value,
+        ];
+
+        foreach ($supports as $support) {
+            $this->assertTrue($this->versionVoter->supportsPublic($support, $versionVoterSubject));
+        }
+
+        $this->assertFalse($this->versionVoter->supportsPublic('invalid_attribute', $versionVoterSubject));
+        $this->assertFalse($this->versionVoter->supportsPublic(VoterSupportConstant::VIEW->value, 'invalid_subject'));
+    }
+}
