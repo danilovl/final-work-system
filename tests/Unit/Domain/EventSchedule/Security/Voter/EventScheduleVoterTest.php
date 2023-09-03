5b6549f0f5c2c88f41b3ddc44379a535109a6c80
Vladimir Danilov <dev.danilovl@gmail.com>
2023-09-03 11:10:00 +0200

    Create EventSchedule Voter test.

---
+++ tests/Unit/Domain/EventSchedule/Security/Voter/EventScheduleVoterTest.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+/*
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Domain\EventSchedule\Security\Voter;
+
+use App\Application\Constant\VoterSupportConstant;
+use App\Domain\EventSchedule\Entity\EventSchedule;
+use App\Domain\EventSchedule\Security\Voter\EventScheduleVoter;
+use App\Tests\Helper\Traits\VoterPublicTrait;
+use PHPUnit\Framework\TestCase;
+
+class EventScheduleVoterTest extends TestCase
+{
+    use VoterPublicTrait;
+
+    private readonly EventScheduleVoter $eventScheduleVoter;
+
+    protected function setUp(): void
+    {
+        $this->eventScheduleVoter = $this->createVoterPublic(EventScheduleVoter::class);
+    }
+
+    public function testSupports(): void
+    {
+        $eventSchedule = $this->createMock(EventSchedule::class);
+
+        $supports = [
+            VoterSupportConstant::VIEW->value,
+            VoterSupportConstant::EDIT->value,
+            VoterSupportConstant::CLONE->value,
+            VoterSupportConstant::DELETE->value
+        ];
+
+        foreach ($supports as $support) {
+            $this->assertTrue($this->eventScheduleVoter->supportsPublic($support, $eventSchedule));
+        }
+
+        $this->assertFalse($this->eventScheduleVoter->supportsPublic('invalid_attribute', $eventSchedule));
+        $this->assertFalse($this->eventScheduleVoter->supportsPublic(VoterSupportConstant::VIEW->value, 'invalid_subject'));
+    }
+}
