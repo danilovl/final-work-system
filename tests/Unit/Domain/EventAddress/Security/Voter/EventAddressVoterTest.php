dac2613fde1c33cd16353e4a1d00f944c9bde4c0
Vladimir Danilov <dev.danilovl@gmail.com>
2023-09-04 11:10:00 +0200

    Create EventAddress Voter test.

---
+++ tests/Unit/Domain/EventAddress/Security/Voter/EventAddressVoterTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+/*
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Domain\EventAddressVoter\Security\Voter;
+
+use App\Application\Constant\VoterSupportConstant;
+use App\Domain\EventAddress\Entity\EventAddress;
+use App\Domain\EventAddress\Security\Voter\EventAddressVoter;
+use App\Tests\Helper\Traits\VoterPublicTrait;
+use PHPUnit\Framework\TestCase;
+
+class EventAddressVoterTest extends TestCase
+{
+    use VoterPublicTrait;
+
+    private readonly EventAddressVoter $eventAddressVoter;
+
+    protected function setUp(): void
+    {
+        $this->eventAddressVoter = $this->createVoterPublic(EventAddressVoter::class);
+    }
+
+    public function testSupports(): void
+    {
+        $eventAddress = $this->createMock(EventAddress::class);
+
+        $supports = [
+            VoterSupportConstant::VIEW->value,
+            VoterSupportConstant::EDIT->value,
+            VoterSupportConstant::DELETE->value
+        ];
+
+        foreach ($supports as $support) {
+            $this->assertTrue($this->eventAddressVoter->supportsPublic($support, $eventAddress));
+        }
+
+        $this->assertFalse($this->eventAddressVoter->supportsPublic('invalid_attribute', $eventAddress));
+        $this->assertFalse($this->eventAddressVoter->supportsPublic(VoterSupportConstant::VIEW->value, 'invalid_subject'));
+    }
+}
