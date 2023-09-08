a4ba27202b74555cd26cf56428d4b6c27778a128
Vladimir Danilov <dev.danilovl@gmail.com>
2023-09-08 11:10:00 +0200

    Create Document Voter test.

---
+++ tests/Unit/Domain/Document/Security/Voter/DocumentVoterTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+/*
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Domain\Document\Security\Voter;
+
+use App\Application\Constant\VoterSupportConstant;
+use App\Domain\Document\Security\Voter\DocumentVoter;
+use App\Domain\Media\Entity\Media;
+use App\Tests\Helper\Traits\VoterPublicTrait;
+use PHPUnit\Framework\TestCase;
+
+class DocumentVoterTest extends TestCase
+{
+    use VoterPublicTrait;
+
+    private readonly DocumentVoter $documentVoter;
+
+    protected function setUp(): void
+    {
+        $this->documentVoter = $this->createVoterPublic(DocumentVoter::class);
+    }
+
+    public function testSupports(): void
+    {
+        $media = $this->createMock(Media::class);
+
+        $supports = [
+            VoterSupportConstant::EDIT->value,
+            VoterSupportConstant::DOWNLOAD->value,
+            VoterSupportConstant::DELETE->value
+        ];
+
+        foreach ($supports as $support) {
+            $this->assertTrue($this->documentVoter->supportsPublic($support, $media));
+        }
+
+        $this->assertFalse($this->documentVoter->supportsPublic('invalid_attribute', $media));
+        $this->assertFalse($this->documentVoter->supportsPublic(VoterSupportConstant::VIEW->value, 'invalid_subject'));
+    }
+}
