08a8bf718dae35a933dfef0e06c159e606e53491
Vladimir Danilov <dev.danilovl@gmail.com>
2023-09-11 11:10:00 +0200

    Create Article Voter test.

---
+++ tests/Unit/Domain/Article/Security/Voter/ArticleVoterTest.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+/*
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Domain\Article\Security\Voter;
+
+use App\Application\Constant\VoterSupportConstant;
+use App\Domain\Article\Security\Voter\ArticleVoter;
+use App\Domain\Article\Security\Voter\Subject\ArticleVoterSubject;
+use App\Domain\ArticleCategory\Security\Voter\ArticleCategoryVoter;
+use App\Tests\Helper\Traits\VoterPublicTrait;
+use PHPUnit\Framework\TestCase;
+
+class ArticleVoterTest extends TestCase
+{
+    use VoterPublicTrait;
+
+    private readonly ArticleVoter $articleVoter;
+
+    protected function setUp(): void
+    {
+        $this->articleVoter = $this->createVoterPublic(ArticleVoter::class);
+    }
+
+    public function testSupports(): void
+    {
+        $articleVoterSubject = $this->createMock(ArticleVoterSubject::class);
+
+        foreach (ArticleCategoryVoter::SUPPORTS as $support) {
+            $this->assertTrue($this->articleVoter->supportsPublic($support, $articleVoterSubject));
+        }
+
+        $this->assertFalse($this->articleVoter->supportsPublic('invalid_attribute', $articleVoterSubject));
+        $this->assertFalse($this->articleVoter->supportsPublic(VoterSupportConstant::VIEW->value, 'invalid_subject'));
+    }
+}
