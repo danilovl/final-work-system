1ebc8368a6d1d0860c54a9f6ffe149b41dcde5bf
Vladimir Danilov <dev.danilovl@gmail.com>
2023-09-14 11:10:00 +0200

    Composer add command. phpunit create testsuite.

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -3,5 +3,6 @@
 test=$1
 
 php tests/ReplacerFinal.php replace
-vendor/bin/phpunit tests/$test
+#XDEBUG_MODE=coverage vendor/bin/phpunit $test --coverage-html coverage-report
+vendor/bin/phpunit $test
 php tests/ReplacerFinal.php replaceBack
