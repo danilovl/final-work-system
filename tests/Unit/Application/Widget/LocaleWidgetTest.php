c318c2c6b9167fd30049d39a567c41d1f8d0f503
Vladimir Danilov <dev.danilovl@gmail.com>
2023-09-30 11:10:00 +0200

    Create LocaleWidgetTest.

---
+++ tests/Unit/Application/Widget/LocaleWidgetTest.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Application\Widget;
+
+use App\Application\Constant\LocaleConstant;
+use App\Application\Service\TwigRenderService;
+use App\Application\Widget\LocalesWidget;
+use PHPUnit\Framework\TestCase;
+
+class LocaleWidgetTest extends TestCase
+{
+    private readonly LocalesWidget $localesWidget;
+
+    protected function setUp(): void
+    {
+        $twigRenderService = $this->createMock(TwigRenderService::class);
+        $twigRenderService->expects($this->any())
+            ->method('render')
+            ->willReturn('twig');
+
+        $this->localesWidget = new LocalesWidget(
+            $twigRenderService,
+            implode('|', LocaleConstant::values())
+        );
+    }
+
+    public function testGetRenderParameters(): void
+    {
+        $expected['locales'] = [
+            [
+                'code' => 'cs',
+                'name' => 'čeština'
+            ],
+            [
+                'code' => 'en',
+                'name' => 'English'
+            ],
+            [
+                'code' => 'ru',
+                'name' => 'русский'
+            ]
+        ];
+
+        $this->assertEquals($expected, $this->localesWidget->getRenderParameters());
+    }
+
+    public function testRender(): void
+    {
+        $this->assertEquals('twig', $this->localesWidget->render());
+    }
+}
