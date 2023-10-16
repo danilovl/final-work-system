ce76a02ffd0aff8ef79c13ba9fc79548b4234d24
Vladimir Danilov <dev.danilovl@gmail.com>
2023-10-16 11:10:00 +0200

    Update WidgetExtensionTest.

--- tests/Unit/Application/Twig/WidgetExtensionTest.php
+++ tests/Unit/Application/Twig/WidgetExtensionTest.php
@@ -13,24 +13,78 @@
 namespace App\Tests\Unit\Application\Twig;
 
 use App\Application\Twig\WidgetExtension;
+use App\Application\Widget\BaseWidget;
 use App\Domain\Widget\Service\WidgetManagerService;
 use PHPUnit\Framework\TestCase;
 use Twig\TwigFunction;
 
 class WidgetExtensionTest extends TestCase
 {
+    private readonly WidgetManagerService $widgetManagerService;
+    private readonly WidgetExtension $widgetExtension;
+
+    protected function setUp(): void
+    {
+        $this->widgetManagerService = $this->createMock(WidgetManagerService::class);
+
+        $this->widgetExtension = new WidgetExtension($this->widgetManagerService);
+    }
+
     public function testGetFunctions(): void
     {
-        $widgetManagerService = $this->createMock(WidgetManagerService::class);
-
-        $seoExtension = new WidgetExtension($widgetManagerService);
         $twigFunction = array_map(static function (TwigFunction $twigFunction): string {
             return $twigFunction->getName();
-        }, $seoExtension->getFunctions());
+        }, $this->widgetExtension->getFunctions());
 
         $this->assertEquals(
             ['widget', 'widget_group', 'widget_homepage_notify'],
             $twigFunction
         );
+    }
+
+    public function testWidget(): void
+    {
+        $widget = new class extends BaseWidget {
+            public function render(): ?string
+            {
+                return 'text widget text';
+            }
+        };
+
+        $this->widgetManagerService
+            ->expects($this->once())
+            ->method('getWidget')
+            ->willReturn($widget);
+
+        $result = $this->widgetExtension->widget('widget');
+
+        $this->assertSame('text widget text', $result);
+    }
+
+    public function testWidgetGroup(): void
+    {
+        $widgetGroup = [
+            new class extends BaseWidget {
+                public function render(): ?string
+                {
+                    return 'text widget one';
+                }
+            },
+            new class extends BaseWidget {
+                public function render(): ?string
+                {
+                    return 'text widget two';
+                }
+            }
+        ];
+
+        $this->widgetManagerService
+            ->expects($this->once())
+            ->method('getWidgetGroup')
+            ->willReturn($widgetGroup);
+
+        $result = $this->widgetExtension->widgetGroup('widget');
+
+        $this->assertSame('text widget onetext widget two', $result);
     }
 }
