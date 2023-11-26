2ec2a8c7464919ebfb75c5e7ebb2a2934484bcac
Vladimir Danilov <dev.danilovl@gmail.com>
2023-11-26 11:10:00 +0100

    Create EmailNotificationMessageTest.

---
+++ tests/Unit/Application/Messenger/EmailNotification/EmailNotificationMessageTest.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Application\Messenger\EmailNotification;
+
+use App\Application\Messenger\EmailNotification\EmailNotificationMessage;
+use PHPUnit\Framework\TestCase;
+
+class EmailNotificationMessageTest extends TestCase
+{
+    public function testGenerateUuid(): void
+    {
+        $emailNotificationMessage = EmailNotificationMessage::createFromArray([
+            'subject' => 'subject',
+            'template' => 'template',
+            'templateParameters' => [
+                'key' => 'value',
+            ],
+            'locale' => 'en',
+            'from' => 'test@example.com',
+            'to' => 'test@example.com',
+            'uuid' => 'uuid',
+        ]);
+
+        $emailNotificationMessage->generateUuid();
+
+        $this->assertSame($emailNotificationMessage->uuid, $emailNotificationMessage->uuid);
+    }
+}
