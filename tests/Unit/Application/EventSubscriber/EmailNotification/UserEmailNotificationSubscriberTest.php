d2b5352274b127e79171ddcaef19a2d25258e0f6
Vladimir Danilov <dev.danilovl@gmail.com>
2023-12-07 11:10:00 +0100

    Create UserEmailNotificationSubscriberTest.

---
+++ tests/Unit/Application/EventSubscriber/EmailNotification/UserEmailNotificationSubscriberTest.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Application\EventSubscriber\EmailNotification;
+
+use App\Application\EventSubscriber\EmailNotification\UserEmailNotificationSubscriber;
+use App\Domain\User\Entity\User;
+use App\Domain\User\EventDispatcher\GenericEvent\UserGenericEvent;
+
+class UserEmailNotificationSubscriberTest extends AbstractBaseEmailNotificationSubscriber
+{
+    protected static string $classSubscriber = UserEmailNotificationSubscriber::class;
+    protected readonly UserEmailNotificationSubscriber $subscriber;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->subscriber = new UserEmailNotificationSubscriber(
+            $this->userFacade,
+            $this->twig,
+            $this->translator,
+            $this->emailNotificationQueueFactory,
+            $this->parameterService,
+            $this->bus
+        );
+    }
+
+    public function testOnResetPasswordToken(): void
+    {
+        $user = new User;
+        $user->setId(1);
+        $user->setEmail('test@example.com');
+        $user->setUsername('username');
+        $user->setPassword('password');
+
+        $event = new UserGenericEvent($user);
+
+        $this->subscriber->onUserCreate($event);
+        $this->subscriber->onUserEdit($event);
+
+        $this->assertTrue(true);
+    }
+}
