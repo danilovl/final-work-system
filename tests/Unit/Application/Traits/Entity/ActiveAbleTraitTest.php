0c3d83eb0be20389bebd370751f75378b2caedc4
Vladimir Danilov <dev.danilovl@gmail.com>
2023-12-18 11:10:00 +0100

    Create ActiveAbleTraitTest.

---
+++ tests/Unit/Application/Traits/Entity/ActiveAbleTraitTest.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Application\Traits\Entity;
+
+use App\Application\Traits\Entity\ActiveAbleTrait;
+use PHPUnit\Framework\TestCase;
+
+class ActiveAbleTraitTest extends TestCase
+{
+    public function testActive(): void
+    {
+        $class = new class {
+            use ActiveAbleTrait;
+        };
+
+        $this->assertFalse($class->isActive());
+
+        $class->setActive(true);
+        $this->assertTrue($class->isActive());
+    }
+}
