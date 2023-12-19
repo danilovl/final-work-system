b03ba7d01e616e4040778e4eb6cd6ad5b23c4cae
Vladimir Danilov <dev.danilovl@gmail.com>
2023-12-19 11:10:00 +0100

    Create ConstantAwareTraitTest.

---
+++ tests/Unit/Application/Traits/Entity/ConstantAwareTraitTest.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Application\Traits\Entity;
+
+use App\Application\Traits\Entity\ConstantAwareTrait;
+use PHPUnit\Framework\TestCase;
+
+class ConstantAwareTraitTest extends TestCase
+{
+    public function testActive(): void
+    {
+        $class = new class {
+            use ConstantAwareTrait;
+        };
+
+        $this->assertNull($class->getConstant());
+
+        $class->setConstant('test');
+        $this->assertSame('test', $class->getConstant());
+    }
+}
