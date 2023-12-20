3204d279f407aa432effa277c2d0ba765dbee709
Vladimir Danilov <dev.danilovl@gmail.com>
2023-12-20 11:10:00 +0100

    Create CreateAbleTraitTest.

---
+++ tests/Unit/Application/Traits/Entity/CreateAbleTraitTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Application\Traits\Entity;
+
+use App\Application\Traits\Entity\CreateAbleTrait;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+class CreateAbleTraitTest extends TestCase
+{
+    public function testActive(): void
+    {
+        $class = new class {
+            use CreateAbleTrait;
+        };
+
+        $date = new DateTime;
+
+        $class->setCreatedAt($date);
+        $this->assertSame($date, $class->getCreatedAt());
+
+        $class->createAblePrePersist();
+
+        $this->assertNotSame($date, $class->getCreatedAt());
+    }
+}
