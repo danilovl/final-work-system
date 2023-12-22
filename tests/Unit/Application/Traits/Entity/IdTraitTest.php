b0083c3402eae58be37961fd296badae74b59633
Vladimir Danilov <dev.danilovl@gmail.com>
2023-12-22 11:10:00 +0100

    Create IdTraitTest.

---
+++ tests/Unit/Application/Traits/Entity/IdTraitTest.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Application\Traits\Entity;
+
+use App\Application\Traits\Entity\IdTrait;
+use PHPUnit\Framework\TestCase;
+
+class IdTraitTest extends TestCase
+{
+    public function testId(): void
+    {
+        $class = new class {
+            use IdTrait;
+        };
+
+        $class->setId(1);
+        $this->assertSame(1, $class->getId());
+
+        $class = clone $class;
+
+        $this->assertNull($class->getId());
+    }
+}
