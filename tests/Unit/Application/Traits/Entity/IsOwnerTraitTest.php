86b4cf812c782a3d594fad2ed8cb55e9c0c5fee6
Vladimir Danilov <dev.danilovl@gmail.com>
2023-12-23 11:10:00 +0100

    Create IsOwnerTraitTest.

---
+++ tests/Unit/Application/Traits/Entity/IsOwnerTraitTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Application\Traits\Entity;
+
+use App\Application\Traits\Entity\IsOwnerTrait;
+use App\Domain\User\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class IsOwnerTraitTest extends TestCase
+{
+    public function testIsOwner(): void
+    {
+        $class = new class {
+            use IsOwnerTrait;
+
+            public function getOwner(): User
+            {
+                $user = new User;
+                $user->setId(1);
+
+                return $user;
+            }
+        };
+
+        $user = new User;
+        $user->setId(2);
+
+        $this->assertFalse($class->isOwner($user));
+
+        $user = new User;
+        $user->setId(1);
+
+        $this->assertTrue($class->isOwner($user));
+    }
+}
