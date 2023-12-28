2d2a6613034b759712a88a6a6a6523597acc9239
Vladimir Danilov <dev.danilovl@gmail.com>
2023-12-28 11:10:00 +0100

    Create SimpleInformationTraitTest.

---
+++ tests/Unit/Application/Traits/Entity/SimpleInformationTraitTest.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Tests\Unit\Application\Traits\Entity;
+
+use App\Application\Traits\Entity\SimpleInformationTrait;
+use PHPUnit\Framework\TestCase;
+
+class SimpleInformationTraitTest extends TestCase
+{
+    public function testIsOwner(): void
+    {
+        $class = new class {
+            use SimpleInformationTrait;
+        };
+
+        $class->setName('name');
+        $this->assertSame('name', $class->getName());
+
+        $class->setDescription('description');
+        $this->assertSame('description', $class->getDescription());
+    }
+}
