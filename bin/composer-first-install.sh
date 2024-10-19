f95c55a06784932e2fb194195eb623c7ac370978
Vladimir Danilov <dev.danilovl@gmail.com>
2024-10-19 11:30:00 +0200

    composer-first-install.sh - remove doctrine:migrations:sync-metadata-storage

--- bin/composer-first-install.sh
+++ bin/composer-first-install.sh
@@ -6,7 +6,6 @@ chmod 777 -R var
 composer install --no-interaction
 php bin/console doctrine:database:create --if-not-exists
 php bin/console doctrine:schema:update --force
-php bin/console doctrine:migrations:sync-metadata-storage
 php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console assets:install public
 php bin/console cache:clear
