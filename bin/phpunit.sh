6a03650768809ba06253d47728702f19f48882d6
Vladimir Danilov <dev.danilovl@gmail.com>
2024-11-11 11:30:00 +0100

    phpunit.sh - add exit phpunit code

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -5,4 +5,7 @@ test=$1
 php tests/ReplacerFinal.php replace
 #XDEBUG_MODE=coverage vendor/bin/phpunit $test --coverage-html coverage-report
 vendor/bin/phpunit $test
+phpunit_exit_code=$?
 php tests/ReplacerFinal.php replaceBack
+
+exit $phpunit_exit_code
