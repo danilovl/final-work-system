6bcc28bb5e193e8a79ec27685b85a8e4bffeb5da
Vladimir Danilov <dev.danilovl@gmail.com>
2024-11-16 11:30:00 +0100

    FileHelperTest - add testDeleteDirectory.

--- src/Application/Helper/FileHelper.php
+++ src/Application/Helper/FileHelper.php
@@ -28,38 +28,6 @@ class FileHelper
 
         return $newName;
     }
-}
-
-declare(strict_types=1);
-
-/*
- *
- * This file is part of the FinalWorkSystem project.
- * (c) Vladimir Danilov
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- */
-
-namespace App\Application\Helper;
-
-class FileHelper
-{
-    public static function createTmpFile(
-        string $extension,
-        string $data,
-        string $prefix = 'temp-file'
-    ): string {
-        /** @var string $file */
-        $file = tempnam(sys_get_temp_dir(), $prefix);
-        $newName = str_replace(pathinfo($file, PATHINFO_EXTENSION), $extension, $file);
-        rename($file, $newName);
-
-        file_put_contents($newName, $data);
-
-        return $newName;
-    }
 
     public static function deleteDirectory(string $dir): bool
     {
--- tests/Unit/Application/Helper/FileHelperTest.php
+++ tests/Unit/Application/Helper/FileHelperTest.php
@@ -17,11 +17,45 @@ use PHPUnit\Framework\TestCase;
 
 class FileHelperTest extends TestCase
 {
+    private string $testDir;
+
+    protected function setUp(): void
+    {
+        $this->testDir = __DIR__ . '/testDir';
+        mkdir($this->testDir);
+
+        file_put_contents($this->testDir . '/file1.txt', 'File 1');
+        file_put_contents($this->testDir . '/file2.txt', 'File 2');
+        mkdir($this->testDir . '/subDir');
+        file_put_contents($this->testDir . '/subDir/file3.txt', 'File 3');
+    }
+
+    protected function tearDown(): void
+    {
+        if (file_exists($this->testDir)) {
+            FileHelper::deleteDirectory($this->testDir);
+        }
+    }
+
     public function testCreateTmpFile(): void
     {
         $fileContent = '<?php echo "test" ?>';
         $filePath = FileHelper::createTmpFile('php', $fileContent, 'prefix');
 
         $this->assertEquals($fileContent, file_get_contents($filePath));
+    }
+
+    public function testDeleteDirectory(): void
+    {
+        $this->assertDirectoryExists($this->testDir);
+
+        $result = FileHelper::deleteDirectory($this->testDir);
+
+        $this->assertTrue($result);
+        $this->assertDirectoryDoesNotExist($this->testDir);
+
+        $result = FileHelper::deleteDirectory($this->testDir);
+        $this->assertFalse($result);
+
     }
 }
