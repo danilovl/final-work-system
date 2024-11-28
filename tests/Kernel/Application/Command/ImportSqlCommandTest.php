f84b806b5890eca150201e84d7ced2858cf1f12f
Vladimir Danilov <dev.danilovl@gmail.com>
2024-11-28 11:30:00 +0100

    ImportSqlCommandTest update create test file.

--- tests/Kernel/Application/Command/ImportSqlCommandTest.php
+++ tests/Kernel/Application/Command/ImportSqlCommandTest.php
@@ -17,11 +17,22 @@ use App\Application\Exception\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
-use Throwable;
 
 class ImportSqlCommandTest extends KernelTestCase
 {
-    private const string SQL_FILE_PATH = __DIR__ . '/data/import-sql-command-data.sql';
+    private string $sqlFilePath;
+
+    protected function setUp(): void
+    {
+        $this->sqlFilePath = $this->createSqlFile();
+    }
+
+    protected function tearDown(): void
+    {
+        if (file_exists($this->sqlFilePath)) {
+            unlink($this->sqlFilePath);
+        }
+    }
 
     public function testExecute(): void
     {
@@ -31,7 +42,7 @@ class ImportSqlCommandTest extends KernelTestCase
         $command = $application->find(ImportSqlCommand::COMMAND_NAME);
         $commandTester = new CommandTester($command);
         $commandTester->execute([
-            'file' => self::SQL_FILE_PATH
+            'file' => $this->sqlFilePath
         ]);
 
         $commandTester->assertCommandIsSuccessful();
@@ -56,15 +67,41 @@ class ImportSqlCommandTest extends KernelTestCase
         $kernel = self::bootKernel();
         $application = new Application($kernel);
 
-        $this->expectException(Throwable::class);
+        $this->expectException(InvalidArgumentException::class);
 
-        $temporaryFilePath = tempnam(sys_get_temp_dir(), 'import-sql-command');
-        chmod($temporaryFilePath, 0000);
+        chmod($this->sqlFilePath, 0000);
 
         $command = $application->find(ImportSqlCommand::COMMAND_NAME);
         $commandTester = new CommandTester($command);
         $commandTester->execute([
-            'file' => $temporaryFilePath
+            'file' => $this->sqlFilePath
         ]);
+    }
+
+    private function createSqlFile(): string
+    {
+        /** @var string $filePath */
+        $filePath = tempnam(sys_get_temp_dir(), 'import-sql-command');
+        file_put_contents($filePath, $this->getSql());
+
+        return $filePath;
+    }
+
+    private function getSql(): string
+    {
+        return "
+            CREATE TABLE IF NOT EXISTS `import_sql_command_test`
+            (
+                `id`        INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                `firstname` VARCHAR(30) NOT NULL,
+                `lastname`  VARCHAR(30) NOT NULL,
+                `email`     VARCHAR(50)
+            );
+            
+            INSERT INTO `import_sql_command_test` (`firstname`, `lastname`, `email`)
+            VALUES ('firstname', 'lastname', 'firstname@email.com');
+            
+            DROP TABLE `import_sql_command_test`;
+        ";
     }
 }
