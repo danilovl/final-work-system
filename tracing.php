f252f7ed115086081e9b78b835f4b2e2acbec605
Vladimir Danilov <dev.danilovl@gmail.com>
2025-07-28 11:30:00 +0200

    tracing.php EventDispatcher - refactoring to auto registration by boot.

--- tracing.php
+++ tracing.php
@@ -1,6 +1,5 @@
 <?php declare(strict_types=1);
 
-use App\Application\Service\EventDispatcherService;
 use Danilovl\AsyncBundle\Service\AsyncService;
 use GuzzleHttp\Exception\{
     ClientException,
@@ -49,58 +48,6 @@ if (extension_loaded('opentelemetry')) {
                 ->createGauge($name, $unit);
         }
     }
-
-    ////////////////////////////EventDispatcherInterface////////////////////////////
-
-    hook(
-        EventDispatcherService::class,
-        'dispatch',
-        pre: static function (EventDispatcherService $dispatcher, array $params, string $class, string $function): void {
-            [$event] = $params;
-            if ($event === null) {
-                return;
-            }
-
-            $parent = Context::getCurrent();
-
-            $instrumentation = new CachedInstrumentation(__CLASS__);
-            $builder = $instrumentation->tracer()
-                ->spanBuilder(sprintf('EVENT DISPATCH %s', $event::class))
-                ->setSpanKind(SpanKind::KIND_INTERNAL)
-                ->setAttribute(TraceAttributes::CODE_FUNCTION, $function)
-                ->setAttribute(TraceAttributes::CODE_NAMESPACE, $class)
-                ->setAttribute('type', 'event');
-
-            $span = $builder->startSpan();
-            $context = $span->storeInContext($parent);
-            Context::storage()->attach($context);
-        },
-        post: static function (EventDispatcherService $dispatcher, array $params, $return, ?Throwable $exception): void {
-            [$event] = $params;
-            if ($event === null) {
-                return;
-            }
-
-            $scope = Context::storage()->scope();
-            if ($scope === null) {
-                return;
-            }
-
-            $scope->detach();
-            $span = Span::fromContext($scope->context());
-
-            if ($exception !== null) {
-                $span->recordException($exception, [
-                    TraceAttributes::EXCEPTION_ESCAPED => true
-                ]);
-                $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
-            } else {
-                $span->setStatus(StatusCode::STATUS_OK);
-            }
-
-            $span->end();
-        }
-    );
 
     ////////////////////////////AsyncService////////////////////////////
 
