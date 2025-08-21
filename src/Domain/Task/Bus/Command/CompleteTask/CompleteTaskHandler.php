6cde5a90a0aef83a32c4de945db7d19f6f7c6d1d
Vladimir Danilov <dev.danilovl@gmail.com>
2025-08-21 11:30:00 +0200

    Create bus CompleteTaskCommand.

---
+++ src/Domain/Task/Bus/Command/CompleteTask/CompleteTaskHandler.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Domain\Task\Bus\Command\CompleteTask;
+
+use App\Application\Interfaces\Bus\CommandHandlerInterface;
+use App\Application\Service\EntityManagerService;
+use App\Domain\Task\Constant\TaskStatusConstant;
+use App\Domain\Task\EventDispatcher\TaskEventDispatcher;
+
+readonly class CompleteTaskHandler implements CommandHandlerInterface
+{
+    public function __construct(
+        private EntityManagerService $entityManagerService,
+        private TaskEventDispatcher $taskEventDispatcher
+    ) {}
+
+    public function __invoke(CompleteTaskCommand $command): void
+    {
+        $task = $command->task;
+
+        $task->changeComplete();
+        $this->entityManagerService->flush();
+
+        $this->taskEventDispatcher->onTaskChangeStatus(
+            $task,
+            TaskStatusConstant::COMPLETE->value
+        );
+    }
+}
--- src/Domain/Task/Http/Ajax/TaskCompleteAllHandle.php
+++ src/Domain/Task/Http/Ajax/TaskCompleteAllHandle.php
@@ -13,12 +13,9 @@
 namespace App\Domain\Task\Http\Ajax;
 
 use App\Application\Constant\AjaxJsonTypeConstant;
-use App\Application\Service\{
-    RequestService,
-    EntityManagerService
-};
-use App\Domain\Task\Constant\TaskStatusConstant;
-use App\Domain\Task\EventDispatcher\TaskEventDispatcher;
+use App\Application\Interfaces\Bus\CommandBusInterface;
+use App\Domain\Task\Bus\Command\CompleteTask\CompleteTaskCommand;
+use App\Application\Service\RequestService;
 use App\Domain\Task\Facade\TaskFacade;
 use App\Domain\User\Service\UserService;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -28,9 +25,8 @@ readonly class TaskCompleteAllHandle
     public function __construct(
         private RequestService $requestService,
         private UserService $userService,
-        private EntityManagerService $entityManagerService,
         private TaskFacade $taskFacade,
-        private TaskEventDispatcher $taskEventDispatcher
+        private CommandBusInterface $commandBus
     ) {}
 
     public function __invoke(): JsonResponse
@@ -43,13 +39,8 @@ readonly class TaskCompleteAllHandle
 
         $tasks = $this->taskFacade->findAllByOwnerComplete($user, false);
         foreach ($tasks as $task) {
-            $task->changeComplete();
-            $this->entityManagerService->flush();
-
-            $this->taskEventDispatcher->onTaskChangeStatus(
-                $task,
-                TaskStatusConstant::COMPLETE->value
-            );
+            $createTaskCommand = new CompleteTaskCommand($task);
+            $this->commandBus->dispatch($createTaskCommand);
         }
 
         return $this->requestService->createAjaxJson(AjaxJsonTypeConstant::SAVE_SUCCESS);
