8fd7daa8682f5ab8b8d46a8859c52d2b4ce46630
Vladimir Danilov <dev.danilovl@gmail.com>
2025-10-26 11:30:00 +0100

    Create QueryBusInterface.

---
+++ src/Application/Interfaces/Bus/QueryBusInterface.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Application\Interfaces\Bus;
+
+use Symfony\Component\Messenger\Exception\ExceptionInterface;
+
+interface QueryBusInterface
+{
+    /**
+     * @throws ExceptionInterface
+     */
+    public function handleResult(QueryInterface $query): object;
+}
---
+++ src/Application/Messenger/MessengerQueryBus.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Application\Messenger;
+
+use App\Application\Interfaces\Bus\{
+    QueryBusInterface,
+    QueryInterface
+};
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
+
+class MessengerQueryBus implements QueryBusInterface
+{
+    public function __construct(private MessageBusInterface $queryBus) {}
+
+    public function handleResult(QueryInterface $query): object
+    {
+        $envelope = $this->queryBus->dispatch($query);
+        $handledStamps = $envelope->all(HandledStamp::class);
+
+        /** @var object $result */
+        $result = $handledStamps[0]->getResult();
+
+        return $result;
+    }
+}
