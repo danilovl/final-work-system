886fb8eca87be53bc45edab028008cf9197d2040
Vladimir Danilov <dev.danilovl@gmail.com>
2026-01-25 11:30:00 +0100

    EmailNotificationCommand - refactoring to new syntax.

--- src/Domain/EmailNotification/Command/EmailNotificationCommand.php
+++ src/Domain/EmailNotification/Command/EmailNotificationCommand.php
@@ -16,46 +16,27 @@ use App\Domain\EmailNotification\Facade\EmailNotificationFacade;
 use App\Domain\EmailNotification\Provider\EmailNotificationSendProvider;
 use App\Domain\EmailNotification\Service\SendEmailNotificationService;
 use App\Infrastructure\Service\EntityManagerService;
-use Override;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 
-class EmailNotificationCommand extends Command
+#[AsCommand(name: 'app:email-notification-send', description: 'Send notification emails')]
+class EmailNotificationCommand
 {
     final public const string COMMAND_NAME = 'app:email-notification-send';
-
-    private SymfonyStyle $io;
 
     public function __construct(
         private readonly EntityManagerService $entityManagerService,
         private readonly EmailNotificationFacade $emailNotificationFacade,
         private readonly SendEmailNotificationService $sendEmailNotificationService,
         private readonly EmailNotificationSendProvider $emailNotificationSendProvider
-    ) {
-        parent::__construct();
-    }
+    ) {}
 
-    #[Override]
-    protected function configure(): void
-    {
-        $this->setName(self::COMMAND_NAME)
-            ->setDescription('Send notification emails');
-    }
-
-    #[Override]
-    protected function initialize(InputInterface $input, OutputInterface $output): void
-    {
-        $this->io = new SymfonyStyle($input, $output);
-    }
-
-    #[Override]
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    public function __invoke(SymfonyStyle $io): int
     {
         if (!$this->emailNotificationSendProvider->isEnable()) {
-            $this->io->error('Email notification sending is unable.');
+            $io->error('Email notification sending is unable.');
 
             return Command::FAILURE;
         }
@@ -64,7 +45,7 @@ class EmailNotificationCommand extends Command
             ->getOneReadyForSender();
 
         if ($emailNotification === null) {
-            $this->io->error('Email notification queue is empty.');
+            $io->error('Email notification queue is empty.');
 
             return Command::FAILURE;
         }
@@ -82,7 +63,7 @@ class EmailNotificationCommand extends Command
         $emailNotification->setSuccess($status);
         $this->entityManagerService->flush();
 
-        $this->io->{$status ? 'success' : 'error'}($message);
+        $io->{$status ? 'success' : 'error'}($message);
 
         return Command::SUCCESS;
     }
