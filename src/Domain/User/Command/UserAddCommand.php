382e582748dc163c5eddc547edfb8301ad7ed64f
Vladimir Danilov <dev.danilovl@gmail.com>
2026-01-26 11:30:00 +0100

    UserAddCommand - refactoring to new syntax.

--- src/Domain/User/Command/UserAddCommand.php
+++ src/Domain/User/Command/UserAddCommand.php
@@ -16,123 +16,115 @@ use App\Application\Helper\HashHelper;
 use App\Infrastructure\Service\EntityManagerService;
 use App\Domain\User\Command\Validator\UserValidator;
 use App\Domain\User\Entity\User;
-use Override;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\RuntimeException;
-use Symfony\Component\Console\Input\{
-    InputArgument,
-    InputInterface
-};
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Stopwatch\Stopwatch;
 
-class UserAddCommand extends Command
+#[AsCommand(name: 'app:user-add', description: 'Creates users and stores them in the database')]
+class UserAddCommand
 {
     final public const string COMMAND_NAME = 'app:user-add';
-
-    private ?SymfonyStyle $io = null;
 
     public function __construct(
         private readonly EntityManagerService $entityManager,
         private readonly UserPasswordHasherInterface $userPasswordHasher,
         private readonly UserValidator $validator
-    ) {
-        parent::__construct();
-    }
+    ) {}
 
-    #[Override]
-    protected function configure(): void
-    {
-        $this->setName(self::COMMAND_NAME)
-            ->setDescription('Creates users and stores them in the database')
-            ->addArgument('username', InputArgument::OPTIONAL, 'The username of the new user')
-            ->addArgument('password', InputArgument::OPTIONAL, 'The plain password of the new user')
-            ->addArgument('email', InputArgument::OPTIONAL, 'The email of the new user')
-            ->addArgument('first-name', InputArgument::OPTIONAL, 'The first name of the new user')
-            ->addArgument('last-name', InputArgument::OPTIONAL, 'The last name of the new user')
-            ->addArgument('roles', InputArgument::OPTIONAL, 'Roles');
-    }
+    public function __invoke(
+        SymfonyStyle $io,
+        OutputInterface $output,
+        #[InputArgument(
+            name: 'username',
+            mode: InputArgument::OPTIONAL,
+            description: 'The username of the new user'
+        )]
+        ?string $username = null,
 
-    #[Override]
-    protected function initialize(InputInterface $input, OutputInterface $output): void
-    {
-        $this->io = new SymfonyStyle($input, $output);
-    }
+        #[InputArgument(
+            name: 'password',
+            mode: InputArgument::OPTIONAL,
+            description: 'The plain password of the new user'
+        )]
+        ?string $password = null,
 
-    #[Override]
-    protected function interact(InputInterface $input, OutputInterface $output): void
-    {
-        if ($this->validInteractInputArguments($input)) {
-            return;
+        #[InputArgument(
+            name: 'email',
+            mode: InputArgument::OPTIONAL,
+            description: 'The email of the new user'
+        )]
+        ?string $email = null,
+
+        #[InputArgument(
+            name: 'first-name',
+            mode: InputArgument::OPTIONAL,
+            description: 'The first name of the new user'
+        )]
+        ?string $firstName = null,
+
+        #[InputArgument(
+            name: 'last-name',
+            mode: InputArgument::OPTIONAL,
+            description: 'The last name of the new user'
+        )]
+        ?string $lastName = null,
+
+        #[InputArgument(
+            name: 'roles',
+            mode: InputArgument::OPTIONAL,
+            description: 'Roles'
+        )]
+        ?string $roles = null
+    ): int {
+        if (!$this->validInteractInputArguments($username, $password, $email, $firstName, $lastName, $roles)) {
+            $io->title('Add user command');
+
+            if ($username !== null) {
+                $io->text(' > <info>Username</info>: ' . $username);
+            } else {
+                $username = $io->ask('Username', null, [$this->validator, 'validateUsername']);
+            }
+
+            if ($email !== null) {
+                $io->text(' > <info>Email</info>: ' . $email);
+            } else {
+                $email = $io->ask('Email', null, [$this->validator, 'validateEmail']);
+            }
+
+            if ($password !== null) {
+                $io->text(' > <info>Password</info>: ' . str_repeat('*', mb_strlen($password)));
+            } else {
+                $password = $io->askHidden('Password (your type will be hidden)', [$this->validator, 'validatePassword']);
+            }
+
+            if ($firstName !== null) {
+                $io->text(' > <info>First Name</info>: ' . $firstName);
+            } else {
+                $firstName = $io->ask('First Name', null, [$this->validator, 'validateFullName']);
+            }
+
+            if ($lastName !== null) {
+                $io->text(' > <info>Last Name</info>: ' . $lastName);
+            } else {
+                $lastName = $io->ask('Last Name', null, [$this->validator, 'validateFullName']);
+            }
+
+            if ($roles !== null) {
+                $io->text(' > <info>Roles(ROLE_USER,ROLE_ADMIN)</info>: ' . $roles);
+            } else {
+                $roles = $io->ask('Roles', null, [$this->validator, 'validateRoles']);
+            }
         }
 
-        $this->io->title('Add user command');
-
-        $username = $input->getArgument('username');
-        if ($username !== null) {
-            $this->io->text(' > <info>Username</info>: ' . $username);
-        } else {
-            $username = $this->io->ask('Username', null, [$this->validator, 'validateUsername']);
-            $input->setArgument('username', $username);
-        }
-
-        $email = $input->getArgument('email');
-        if ($email !== null) {
-            $this->io->text(' > <info>Email</info>: ' . $email);
-        } else {
-            $email = $this->io->ask('Email', null, [$this->validator, 'validateEmail']);
-            $input->setArgument('email', $email);
-        }
-
-        $password = $input->getArgument('password');
-        if ($password !== null) {
-            $this->io->text(' > <info>Password</info>: ' . str_repeat('*', mb_strlen($password)));
-        } else {
-            $password = $this->io->askHidden('Password (your type will be hidden)', [$this->validator, 'validatePassword']);
-            $input->setArgument('password', $password);
-        }
-
-        $firstName = $input->getArgument('first-name');
-        if ($firstName !== null) {
-            $this->io->text(' > <info>First Name</info>: ' . $firstName);
-        } else {
-            $firstName = $this->io->ask('First Name', null, [$this->validator, 'validateFullName']);
-            $input->setArgument('first-name', $firstName);
-        }
-
-        $lastName = $input->getArgument('last-name');
-        if ($lastName !== null) {
-            $this->io->text(' > <info>Last Name</info>: ' . $lastName);
-        } else {
-            $lastName = $this->io->ask('Last Name', null, [$this->validator, 'validateFullName']);
-            $input->setArgument('last-name', $lastName);
-        }
-
-        $roles = $input->getArgument('roles');
-        if ($roles !== null) {
-            $this->io->text(' > <info>Roles(ROLE_USER,ROLE_ADMIN)</info>: ' . $roles);
-        } else {
-            $roles = $this->io->ask('Roles', null, [$this->validator, 'validateRoles']);
-            $input->setArgument('roles', $roles);
-        }
-    }
-
-    #[Override]
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
         $stopwatch = new Stopwatch;
         $stopwatch->start('add-user-command');
 
-        $username = $input->getArgument('username');
-        $email = $input->getArgument('email');
-        $plainPassword = $input->getArgument('password');
-        $firstName = $input->getArgument('first-name');
-        $lastName = $input->getArgument('last-name');
-        $roles = $input->getArgument('roles');
-
-        $this->validateUserData($username, $plainPassword, $email, $firstName, $lastName);
+        $this->validateUserData($username, $password, $email, $firstName, $lastName);
 
         $user = new User;
         $user->setEnabled(true);
@@ -144,16 +136,16 @@ class UserAddCommand extends Command
         $user->setRoles(explode(',', $roles));
         $user->setSalt(HashHelper::generateUserSalt());
 
-        $encodedPassword = $this->userPasswordHasher->hashPassword($user, $plainPassword);
+        $encodedPassword = $this->userPasswordHasher->hashPassword($user, $password);
         $user->setPassword($encodedPassword);
 
         $this->entityManager->persistAndFlush($user);
 
-        $this->io->success(sprintf('User was successfully created: %s (%s)', $user->getUsername(), $user->getEmail()));
+        $io->success(sprintf('User was successfully created: %s (%s)', $user->getUsername(), $user->getEmail()));
 
         $event = $stopwatch->stop('add-user-command');
         if ($output->isVerbose()) {
-            $this->io->comment(
+            $io->comment(
                 sprintf('New user database id: %d / Elapsed time: %.2f ms / Consumed memory: %.2f MB',
                     $user->getId(),
                     $event->getDuration(),
@@ -187,14 +179,20 @@ class UserAddCommand extends Command
         $this->validator->validateFullName($lastName);
     }
 
-    private function validInteractInputArguments(InputInterface $input): bool
-    {
-        foreach (['username', 'password', 'email', 'first-name', 'last-name', 'roles'] as $argument) {
-            if ($input->getArgument($argument) === null) {
-                return false;
-            }
-        }
-
-        return true;
+    private function validInteractInputArguments(
+        ?string $username,
+        ?string $password,
+        ?string $email,
+        ?string $firstName,
+        ?string $lastName,
+        ?string $roles
+    ): bool {
+        return $username !== null &&
+            $password !== null &&
+            $email !== null &&
+            $firstName !== null &&
+            $lastName !== null &&
+            $roles !== null;
     }
+
 }
