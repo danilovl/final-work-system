4e302fa21f048d58bd3f9549c1acf2fc337942ec
Vladimir Danilov <dev.danilovl@gmail.com>
2026-05-04 11:30:00 +0200

    Composer install nelmio/api-doc-bundle.

--- config/bundles.php
+++ config/bundles.php
@@ -24,6 +24,7 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Danilovl\ParameterBundle\ParameterBundle::class => ['all' => true],
     Danilovl\TranslatorBundle\TranslatorBundle::class => ['all' => true],
     Danilovl\RenderServiceTwigExtensionBundle\RenderServiceTwigExtensionBundle::class => ['all' => true],
