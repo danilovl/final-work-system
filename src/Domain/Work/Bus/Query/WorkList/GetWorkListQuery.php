437fb0ad6149ea06fadd23ef9efc9efe3947cfc7
Vladimir Danilov <dev.danilovl@gmail.com>
2026-07-06 11:30:00 +0200

    Create GetWorkListQuery.

---
+++ src/Domain/Work/Bus/Query/WorkList/GetWorkListQuery.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Domain\Work\Bus\Query\WorkList;
+
+use App\Application\Interfaces\Bus\QueryInterface;
+use App\Domain\User\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\Attribute\AsMessage;
+
+#[AsMessage('sync')]
+readonly class GetWorkListQuery implements QueryInterface
+{
+    private function __construct(
+        public Request $request,
+        public User $user,
+        public string $type,
+        public array $search
+    ) {}
+
+    public static function create(
+        Request $request,
+        User $user,
+        string $type,
+        array $search
+    ): self {
+        return new self(
+            request: $request,
+            user: $user,
+            type: $type,
+            search: $search
+        );
+    }
+}
---
+++ src/Domain/Work/Bus/Query/WorkList/GetWorkListQueryHandler.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Domain\Work\Bus\Query\WorkList;
+
+use App\Application\Interfaces\Bus\QueryHandlerInterface;
+use App\Domain\Work\Repository\Elastica\ElasticaWorkRepository;
+use App\Infrastructure\Service\PaginatorService;
+
+readonly class GetWorkListQueryHandler implements QueryHandlerInterface
+{
+    public function __construct(
+        private PaginatorService $paginatorService,
+        private ElasticaWorkRepository $elasticaWorkRepository
+    ) {}
+
+    public function __invoke(GetWorkListQuery $query): GetWorkListQueryResult
+    {
+        $works = $this->elasticaWorkRepository->filterWorkList(
+            $query->user,
+            $query->type,
+            $query->search
+        );
+
+        $pagination = $this->paginatorService->createPaginationRequest($query->request, $works);
+
+        return new GetWorkListQueryResult(works: $pagination);
+    }
+}
