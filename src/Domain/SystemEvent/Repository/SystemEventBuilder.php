8153db0405e74e09be369a4196a2133a4dbc6a80
Vladimir Danilov <dev.danilovl@gmail.com>
2026-09-15 11:30:00 +0200

    Create SystemEventQueryBuilder.

---
+++ src/Domain/SystemEvent/Repository/SystemEventBuilder.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * This file is part of the FinalWorkSystem project.
+ * (c) Vladimir Danilov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace App\Domain\SystemEvent\Repository;
+
+use App\Domain\User\Entity\User;
+use App\Infrastructure\Persistence\Doctrine\Repository\BaseQueryBuilder;
+use Doctrine\Common\Collections\Order;
+
+class SystemEventBuilder extends BaseQueryBuilder
+{
+    public function leftJoinAll(): self
+    {
+        $this->queryBuilder
+            ->leftJoin('system_event.type', 'type')
+            ->leftJoin('system_event.recipient', 'recipient')
+            ->leftJoin('system_event.conversation', 'conversation')
+            ->leftJoin('system_event.event', 'event')
+            ->leftJoin('system_event.owner', 'owner')
+            ->leftJoin('system_event.task', 'task')
+            ->leftJoin('system_event.work', 'work');
+
+        return $this;
+    }
+
+    public function distinct(): self
+    {
+        $this->queryBuilder->distinct();
+
+        return $this;
+    }
+
+    public function orderByCreatedAt(string $order = Order::Descending->value): self
+    {
+        $this->queryBuilder->orderBy('system_event.createdAt', $order);
+
+        return $this;
+    }
+
+    public function byRecipient(User $recipient): self
+    {
+        $this->queryBuilder
+            ->where('recipient.recipient = :recipient')
+            ->setParameter('recipient', $recipient);
+
+        return $this;
+    }
+
+    public function byRecipientViewed(bool $viewed): self
+    {
+        $this->queryBuilder
+            ->andWhere('recipient.viewed = :viewed')
+            ->setParameter('viewed', $viewed);
+
+        return $this;
+    }
+
+    public function selectCount(): self
+    {
+        $this->queryBuilder->select('count(system_event.id)');
+
+        return $this;
+    }
+}
--- src/Domain/SystemEvent/Repository/SystemEventRepository.php
+++ src/Domain/SystemEvent/Repository/SystemEventRepository.php
@@ -15,7 +15,6 @@ namespace App\Domain\SystemEvent\Repository;
 use App\Domain\SystemEvent\Entity\SystemEvent;
 use App\Domain\User\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Collections\Order;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\QueryBuilder;
 
@@ -26,36 +25,30 @@ class SystemEventRepository extends ServiceEntityRepository
         parent::__construct($registry, SystemEvent::class);
     }
 
-    private function createBaseQueryBuilder(): QueryBuilder
+    private function createSystemEventBuilder(): SystemEventBuilder
     {
-        return $this->createQueryBuilder('system_event')
-            ->leftJoin('system_event.type', 'type')
-            ->leftJoin('system_event.recipient', 'recipient')
-            ->leftJoin('system_event.conversation', 'conversation')
-            ->leftJoin('system_event.event', 'event')
-            ->leftJoin('system_event.owner', 'owner')
-            ->leftJoin('system_event.task', 'task')
-            ->leftJoin('system_event.work', 'work');
+        return new SystemEventBuilder($this->createQueryBuilder('system_event'));
     }
 
     public function allByRecipient(User $recipient): QueryBuilder
     {
-        return $this->createBaseQueryBuilder()
+        return $this->createSystemEventBuilder()
+            ->leftJoinAll()
             ->distinct()
-            ->where('recipient.recipient = :recipient')
-            ->orderBy('system_event.createdAt', Order::Descending->value)
-            ->setParameter('recipient', $recipient);
+            ->byRecipient($recipient)
+            ->orderByCreatedAt()
+            ->getQueryBuilder();
     }
 
     public function countUnreadByRecipient(User $recipient): QueryBuilder
     {
-        return $this->createBaseQueryBuilder()
+        return $this->createSystemEventBuilder()
+            ->leftJoinAll()
             ->distinct()
-            ->select('count(system_event.id)')
-            ->where('recipient.recipient = :recipient')
-            ->andWhere('recipient.viewed = :viewed')
-            ->orderBy('system_event.createdAt', Order::Descending->value)
-            ->setParameter('recipient', $recipient)
-            ->setParameter('viewed', false);
+            ->selectCount()
+            ->byRecipient($recipient)
+            ->byRecipientViewed(false)
+            ->orderByCreatedAt()
+            ->getQueryBuilder();
     }
 }
